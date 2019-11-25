510cbc2a16386a81109991355f6c493f44e3e776
marcegarzab <marcegarzab@gmail.com>
2019-11-25 17:28:47 -0600

    Add correct files

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const router = require('./routes')
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 //ya esta checando
 
 app.use(cors())
 app.use(express.json()) // parsea a json
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 // revisa tu connectionURL aqui :-)
+var connectionURL = 'mongodb+srv://admin:admin@marcelacluster-qiczh.mongodb.net/Exam3?retryWrites=true&w=majority'
+
+// var connectionURL = '../config.js'
+
 
 mongoose.connect( connectionURL, {
   useNewUrlParser: true,
